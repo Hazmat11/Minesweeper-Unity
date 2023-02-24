6842b9cdae56b18d1676f462c7312e54491eb2e6
Hazmat11 <ebertrand@gaming.tech>
2023-02-24 10:37:41 +0100

    Bomb difference

--- Assets/Scripts/DifficultyLevel.cs
+++ Assets/Scripts/DifficultyLevel.cs
@@ -6,6 +6,7 @@ public class DifficultyLevel : MonoBehaviour
 {
     public int width;
     public int height;
+    public int nbMine;
     private GameObject level;
 
     private void Start()
@@ -17,16 +18,19 @@ public class DifficultyLevel : MonoBehaviour
     {    
         level.GetComponent<DifficultyLevel>().width = 9;
         level.GetComponent<DifficultyLevel>().height = 9;
+        level.GetComponent<DifficultyLevel>().nbMine = 10;
     }
     public void Medium()
     {
         level.GetComponent<DifficultyLevel>().width = 16;
         level.GetComponent<DifficultyLevel>().height = 16;
+        level.GetComponent<DifficultyLevel>().nbMine = 40;
     }
     public void Hard()
     {
         level.GetComponent<DifficultyLevel>().width = 16;
         level.GetComponent<DifficultyLevel>().height = 30;
+        level.GetComponent<DifficultyLevel>().nbMine = 99;
     }
 
     void Update()
